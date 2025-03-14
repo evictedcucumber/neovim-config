6fa5c19bf8587d6bb91c2d941fd6f2d50f01e16c
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-14 22:13:37 +0200

    feat: remove telescope from colourscheme highlights

--- lua/me/plugins.lua
+++ lua/me/plugins.lua
@@ -17,16 +17,6 @@ return {
                     CurSearch = { fg = 'base', bg = 'leaf', inherit = false },
                     Search = { fg = 'text', bg = 'leaf', blend = 20, inherit = false },
                     NotifyBackground = { bg = '#000000', inherit = true },
-                    TelescopeBorder = { fg = 'overlay', bg = 'overlay' },
-                    TelescopeNormal = { fg = 'subtle', bg = 'overlay' },
-                    TelescopeSelection = { fg = 'text', bg = 'highlight_med' },
-                    TelescopeSelectionCaret = { fg = 'love', bg = 'highlight_med' },
-                    TelescopeMultiSelection = { fg = 'text', bg = 'highlight_high' },
-                    TelescopeTitle = { fg = 'base', bg = 'love' },
-                    TelescopePromptTitle = { fg = 'base', bg = 'pine' },
-                    TelescopePreviewTitle = { fg = 'base', bg = 'iris' },
-                    TelescopePromptNormal = { fg = 'text', bg = 'surface' },
-                    TelescopePromptBorder = { fg = 'surface', bg = 'surface' },
                 },
             })
             vim.cmd('colorscheme rose-pine-main')
