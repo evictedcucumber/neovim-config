298125f9091e14fd5d3a00514678f6b00b741ba4
evictedcucumber <95688781+evictedcucumber@users.noreply.github.com>
2025-04-10 08:27:41 +0000

    feat: Add nix flakes to use `nix develop`

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/release-24.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ zsh stylua luajitPackages.luacheck pre-commit ];
+
+          shellHook = ''
+            if [[ "$(basename "$0")" != "zsh" ]]; then
+                exec ${pkgs.zsh}/bin/zsh -l
+            fi
+          '';
+        };
+
+      });
+}
