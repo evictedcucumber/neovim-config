21630ed100fc3a49179e529c26aded5c8a50891b
evictedcucumber <95688781+evictedcucumber@users.noreply.github.com>
2025-04-13 20:49:17 +0000

    fix: Conform notification now uses Snacks

--- lua/me/configs/conform.lua
+++ lua/me/configs/conform.lua
@@ -13,7 +13,7 @@ conform.setup({
 vim.keymap.set({ 'n', 'v' }, '<leader>fm', function()
     conform.format({ timeout_ms = 3000, lsp_format = 'fallback' }, function(err)
         if not err then
-            vim.notify('Successfully formatted.')
+            Snacks.notify('Successfully formatted.')
             vim.cmd('write')
         end
     end)
