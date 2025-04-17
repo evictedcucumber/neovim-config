a90432803b965c12caff3dc4b7a0d986d5dcd537
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-04-17 09:55:49 +0000

    format flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,21 @@
 {
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        name = "neovim-config";
-        packages = with pkgs; [
-          stylua
-          luajitPackages.luacheck
-          lua-language-server
-          pre-commit
-        ];
-      };
-
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
     };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "neovim-config";
+      packages = with pkgs; [
+        stylua
+        luajitPackages.luacheck
+        lua-language-server
+        pre-commit
+      ];
+    };
+  };
 }
