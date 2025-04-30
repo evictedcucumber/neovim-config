c6d4382459110be4134405a65e6b1fa64c8d4517
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-04-30 22:00:48 +0000

    Added correct rust nightly

--- flake.nix
+++ flake.nix
@@ -1,16 +1,28 @@
 {
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    ...
+  }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
       config.allowUnfree = true;
+
+      overlays = [(import rust-overlay)];
     };
   in {
     devShells.${system}.default = pkgs.mkShell {
       name = "neovim-config";
-      packages = with pkgs; [stylua lua-language-server];
+      packages = with pkgs; [
+        stylua
+        lua-language-server
+        (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+      ];
     };
   };
 }
