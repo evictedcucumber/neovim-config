4f2be8e7fd3b99f419bc8181bc7d6f3e34411aff
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-05-22 11:41:09 +0000

    fixed yazi keymaps

--- lua/me/plugins/yazi.lua
+++ lua/me/plugins/yazi.lua
@@ -1,10 +1,10 @@
 return {
     'mikavilpas/yazi.nvim',
     dependencies = 'folke/snacks.nvim',
-    event = 'VeryLazy',
+    lazy = false,
     keys = {
-        { '_', '<C>Yazi cwd<CR>', desc = 'Open Yazi in CWD' },
-        { '-', '<C>Yazi<CR>', desc = 'Open Yazi at current file' },
+        { '_', '<CMD>Yazi cwd<CR>', desc = 'Open Yazi in CWD' },
+        { '-', '<CMD>Yazi<CR>', desc = 'Open Yazi at current file' },
     },
     init = function()
         vim.g.loaded_netrwPlugin = 1
