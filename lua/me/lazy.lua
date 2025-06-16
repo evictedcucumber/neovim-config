0f09e5fab3578d577c055196a14e077eeebf6be7
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-06-16 12:40:43 +0000

    remove compile plugin for my easycmd plugin

--- lua/me/lazy.lua
+++ lua/me/lazy.lua
@@ -15,6 +15,20 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
     spec = {
         { import = 'me.plugins' },
+        {
+            dir = '~/repos/easycmd.nvim',
+            cmd = 'EasyCmd',
+            keys = {
+                { '<leader>cc', '<cmd>EasyCmd run 1<CR>', desc = 'Run [C]omplie [C]ommand' },
+                { '<leader>cC', '<cmd>EasyCmd edit 1<CR>', desc = 'Edit [C]omplie [C]ommand' },
+                { '<leader>cr', '<cmd>EasyCmd run 2 vsplit<CR>', desc = 'Run [C]ommand [R]un' },
+                { '<leader>cR', '<cmd>EasyCmd edit 2<CR>', desc = 'Edit [C]ommand [R]un' },
+                { '<leader>ct', '<cmd>EasyCmd run 3<CR>', desc = 'Run [C]ommand [T]est' },
+                { '<leader>cT', '<cmd>EasyCmd edit 3<CR>', desc = 'Edit [C]ommand [T]est' },
+            },
+            ---@type easycmd.Config
+            opts = {},
+        },
     },
     install = {
         missing = true,
