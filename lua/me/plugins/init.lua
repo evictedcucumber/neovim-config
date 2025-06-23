ac29d35b210bdd15221654744e9a6670d9e47072
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-06-23 13:07:28 +0000

    removed hardtime.nvim

--- lua/me/plugins/init.lua
+++ lua/me/plugins/init.lua
@@ -5,12 +5,6 @@ return {
         ft = 'lua',
         opts = {},
     },
-    {
-        'm4xshen/hardtime.nvim',
-        lazy = false,
-        dependencies = { 'MunifTanjim/nui.nvim' },
-        opts = {},
-    },
     { 'nvim-tree/nvim-web-devicons', lazy = false, opts = {} },
     { 'nvim-lua/plenary.nvim', lazy = false },
     { 'echasnovski/mini.icons', lazy = false, version = false, opts = {} },
