8773ab0c963a528f964380398deb76673f04356f
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-08-23 20:54:22 +0000

    feat(plugins): added basic plugins

---
+++ lua/me/plugins.lua
@@ -0,0 +1,28 @@
+vim.pack.add({
+    -- global dependencies
+    'https://github.com/nvim-lua/plenary.nvim',
+    'https://github.com/nvim-tree/nvim-web-devicons',
+    'https://github.com/echasnovski/mini.icons',
+}, { confirm = false })
+
+local post_update_or_install_group =
+    vim.api.nvim_create_augroup('PackPostUpdateOrInstall', { clear = true })
+vim.api.nvim_create_autocmd('User', {
+    group = post_update_or_install_group,
+    pattern = 'PackChanged',
+    callback = function(args)
+        local data = args.data or {}
+        local spec = data.spec or {}
+        local name = spec.name or ''
+        local src = spec.src or ''
+
+    end,
+})
+
+vim.api.nvim_create_user_command('UpdatePlugins', function()
+    vim.pack.update()
+end, {})
+
+require('nvim-web-devicons').setup({})
+require('mini.icons').setup({})
+
