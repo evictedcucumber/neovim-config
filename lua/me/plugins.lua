751f203e730757f567c14b95cca720f50f583cdb
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-08-25 09:00:08 +0000

    feat(plugins): added matching simple plugin management from utils

--- lua/me/plugins.lua
+++ lua/me/plugins.lua
@@ -1,46 +1,41 @@
-vim.pack.add({
-    -- global dependencies
-    'https://github.com/nvim-lua/plenary.nvim',
-    'https://github.com/nvim-tree/nvim-web-devicons',
-    'https://github.com/echasnovski/mini.icons',
-    { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
-    'https://github.com/folke/snacks.nvim',
-    {
-        src = 'https://github.com/nvim-treesitter/nvim-treesitter',
-        version = 'main',
-    },
-    'https://github.com/nvim-treesitter/nvim-treesitter-context',
-}, { confirm = false })
+local add_plugin = require('me.util').add_plugin
+local add_plugins = require('me.util').add_plugins
+local safe_setup = require('me.util').safe_setup
+local add_post_pack_install_update_hook =
+    require('me.util').add_post_pack_install_update_hook
 
 local post_update_or_install_group =
     vim.api.nvim_create_augroup('PackPostUpdateOrInstall', { clear = true })
-vim.api.nvim_create_autocmd('User', {
+vim.api.nvim_create_autocmd('PackChanged', {
     group = post_update_or_install_group,
-    pattern = 'PackChanged',
     callback = function(args)
         local data = args.data or {}
+        local kind = data.kind or ''
         local spec = data.spec or {}
         local name = spec.name or ''
-        local src = spec.src or ''
+        local path = data.path
 
-        if data.kind == 'install' or data.kind == 'update' then
-            vim.notify('matched kind')
-            if
-                name == 'nvim-treesitter'
-                or src:find('nvim%-treesitter/nvim%-treesitter')
-            then
-                vim.notify('matched treesitter')
-                vim.schedule(function()
-                    vim.cmd('TSUpdate')
-                end)
+        if kind == 'install' or kind == 'update' then
+            for _, entry in
+                ipairs(require('me.util').post_pack_install_update_hooks)
+            do
+                local hooked_name, hook = entry.name, entry.hook
+                if hooked_name == name then
+                    vim.cmd('packadd ' .. hooked_name)
+                    local ok, _ = pcall(hook, path)
+                    if not ok then
+                        vim.print(
+                            'Unable to call hook function for `'
+                                .. hooked_name
+                                .. '`.'
+                        )
+                    end
+                end
             end
         end
     end,
 })
 
-vim.api.nvim_create_user_command('UpdatePlugins', function()
-    vim.pack.update()
-end, {})
 
 require('nvim-web-devicons').setup({})
 require('mini.icons').setup({})
