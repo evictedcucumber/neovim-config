5d992e6c16059c8fd6de7915806cc2f3cb9c57c8
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-09-18 09:28:37 +0000

    feat(markdown): enabled wrap and keymaps for wrap navigation

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,41 @@
-vim.wo.wrap = false
+vim.wo.wrap = true
+vim.wo.linebreak = true
+vim.wo.colorcolumn = ''
+vim.wo.number = false
+vim.wo.relativenumber = false
+vim.wo.signcolumn = 'no'
 
 vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
+
+vim.keymap.set(
+    { 'n', 'x', 'o' },
+    'j',
+    'gj',
+    { buffer = true, noremap = true, silent = true }
+)
+vim.keymap.set(
+    { 'n', 'x', 'o' },
+    'k',
+    'gk',
+    { buffer = true, noremap = true, silent = true }
+)
+vim.keymap.set(
+    { 'n', 'x', 'o' },
+    '0',
+    'g0',
+    { buffer = true, noremap = true, silent = true }
+)
+vim.keymap.set(
+    { 'n', 'x', 'o' },
+    '^',
+    'g^',
+    { buffer = true, noremap = true, silent = true }
+)
+vim.keymap.set(
+    { 'n', 'x', 'o' },
+    '$',
+    'g$',
+    { buffer = true, noremap = true, silent = true }
+)
