71a2618bd023dc4c1e98313c56fc23c4d72488f7
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-05-19 19:57:24 +0200

    chore: formatting

--- lsp/hyprls.lua
+++ lsp/hyprls.lua
@@ -9,7 +9,7 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'hyprls', '--stdio' },
-  filetypes = { 'hyprlang' },
-  root_markers = { '.git' },
+    cmd = { 'hyprls', '--stdio' },
+    filetypes = { 'hyprlang' },
+    root_markers = { '.git' },
 }
