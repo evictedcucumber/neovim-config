6d90a690e0605efe0c64ab7ca695d0ca158f09b4
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-05-30 11:34:16 +0200

    fix: hledger align commodity keymap

--- lua/me/plugins/filetype/hledger.lua
+++ lua/me/plugins/filetype/hledger.lua
@@ -10,7 +10,7 @@ vim.api.nvim_create_autocmd('FileType', {
         _G.keymap(
             'n',
             '<leader>la',
-            '<cmd>call ledger#align_commodity_buffer',
+            '<cmd>call ledger#align_commodity_buffer()<CR>',
             { desc = 'Align Ledger Commodities' }
         )
     end,
