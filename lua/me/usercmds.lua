75384bb1ee097aff02b6d83cfa1b90713f77de69
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-07-21 17:34:19 +0200

    feat: remove packupdate command

--- lua/me/usercmds.lua
+++ lua/me/usercmds.lua
@@ -1,5 +1,3 @@
-vim.api.nvim_create_user_command('PackUpdate', 'lua vim.pack.update()', {})
-
 vim.api.nvim_create_user_command(
     'BlinkInstall',
     'lua require("blink.cmp").build():wait(60000)',
